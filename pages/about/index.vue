5681bf3df5f0f07857111b4a167f925814d795bd
José Garcia <jose.ragarcia82@gmail.com>
2021-11-07 15:43:56 -0300

    New post page created

---
+++ pages/about/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about-page">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+      reiciendis consequatur atque, totam inventore sed at minus vitae, cumque,
+      necessitatibus quis recusandae vero nulla. Distinctio eveniet quos sit
+      similique ipsa.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+      reiciendis consequatur atque, totam inventore sed at minus vitae, cumque,
+      necessitatibus quis recusandae vero nulla. Distinctio eveniet quos sit
+      similique ipsa.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+      reiciendis consequatur atque, totam inventore sed at minus vitae, cumque,
+      necessitatibus quis recusandae vero nulla. Distinctio eveniet quos sit
+      similique ipsa.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+      reiciendis consequatur atque, totam inventore sed at minus vitae, cumque,
+      necessitatibus quis recusandae vero nulla. Distinctio eveniet quos sit
+      similique ipsa.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.aboute-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
